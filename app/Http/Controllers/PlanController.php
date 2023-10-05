607f2a206dd8ef0f37b0d3fb67be6366ddc448a4
shobidayo <aobasho8@gmail.com>
2023-10-05 10:45:53 +0900

    人数で検索したら呼び出されるようにした

--- app/Http/Controllers/PlanController.php
+++ app/Http/Controllers/PlanController.php
@@ -7,6 +7,13 @@ use App\Models\Plan;
 
 class PlanController extends Controller
 {
+    public function search(Request $request, Plan $plans)
+    {
+        $peopleNumber= $request->input('people');
+        $selected_plans=$plans->rooms()->where('capacity',$peopleNumber)->get();
+        //{{ $selected_plans->rooms->capacity }}
+    }
+    
     public function choices(Plan $plan)
     {
         return view('plans.choose')->with(['choices'=>$plan->getPaginateBylimit(3)]);
