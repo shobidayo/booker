ee70f77cb4eff4a94c2d0bdb342f9ec9bd44a424
shobidayo <aobasho8@gmail.com>
2023-10-09 07:34:44 +0900

    reservation_plan,roomのシーダーファイル作成と入力

---
+++ database/seeders/ReservationRoomSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use DateTime;
+
+class ReservationRoomSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('reservation_room')->insert([
+            'reservation_id'=>2,
+            'room_id'=>1,
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('reservation_room')->insert([
+            'reservation_id'=>3,
+            'room_id'=>1,
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('reservation_room')->insert([
+            'reservation_id'=>4,
+            'room_id'=>2,
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('reservation_room')->insert([
+            'reservation_id'=>5,
+            'room_id'=>2,
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+    }
+}
